e4a1e22d342e1ce9d17f9e9f7e37cc6c23581774
Rafael Guedes <r.guedes@oceanum.science>
2024-09-20 14:40:04 +1200

    Add rasterio source

--- src/rompy_xbeach/source.py
+++ src/rompy_xbeach/source.py
@@ -3,7 +3,8 @@
 import logging
 from pathlib import Path
 from typing import Literal
-from pydantic import Field
+from pydantic import Field, field_validator
+import rioxarray
 import xarray as xr
 
 from rompy.core.source import SourceBase
@@ -14,30 +15,38 @@ logger = logging.getLogger(__name__)
 HERE = Path(__file__).parent
 
 
-class XbeachSource(SourceBase):
-    """Xbeach source class.
-
-    This is a dummy class to demonstrate how to create a source type to use with the
-    Data objects. The functionality in this class is already provided by the SourceFile
-    class in rompy.core.source so this class is not necessary. It is included here as
-    an example.
-
-    """
+class SourceRasterio(SourceBase):
+    """Rioxarray source class."""
 
     model_type: Literal["xbeach"] = Field(
         default="xbeach",
         description="Model type discriminator",
     )
-    uri: str | Path = Field(description="Path to the dataset")
+    filename: str | Path = Field(description="Path to the rasterio dataset")
+    band: int = Field(
+        default=1,
+        description="Band to read from the rasterio dataset",
+        ge=1,
+    )
     kwargs: dict = Field(
         default={},
-        description="Keyword arguments to pass to xarray.open_dataset",
+        description="Keyword arguments to pass to rioxarray.open_rasterio",
     )
 
-    def __str__(self) -> str:
-        """String representation for this source class."""
-        return f"SourceFile(uri={self.uri})"
+    @field_validator("kwargs")
+    @classmethod
+    def validate_rasterio_kwargs(cls, v) -> dict:
+        """Validate the rasterio keyword arguments."""
+        # Ensure Dataset is not returned
+        v["band_as_variable"] = False
+        # Ensure band name is not set
+        if "default_name" in v:
+            logger.info("Ignoring `default_name` from rasterio kwargs")
+            v.pop("default_name")
+        return v
 
     def _open(self) -> xr.Dataset:
         """This method needs to return an xarray Dataset object."""
-        return xr.open_dataset(self.uri, **self.kwargs)
+        xds = rioxarray.open_rasterio(self.filename, **self.kwargs)
+        xds = xds.sel(band=self.band).to_dataset(name="data")
+        return xds
